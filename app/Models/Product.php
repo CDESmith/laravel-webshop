db06420dc89bb501eeef050de1a92b0d6b7cc415
Callum D E Smith <cdes@cdesmith.co.uk>
2024-04-12 12:46:15 +0100

    Added handling of currency formatting.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,11 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Livewire\Attribute;
+use Money\Currency;
+use Money\Money;
 
 class Product extends Model
 {
     use HasFactory;
-
+    protected function price(): \Illuminate\Database\Eloquent\Casts\Attribute
+    {
+        return \Illuminate\Database\Eloquent\Casts\Attribute::make(
+          get: function (int $value) {
+              return new Money($value, new Currency('GBP'));
+            }
+        );
+    }
     public function variants(): HasMany
     {
         return $this->hasMany(ProductVariant::class);
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\IntlMoneyFormatter;
+use Money\Money;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::stringable(function(Money $money) {
+          $currencies = new ISOCurrencies();
+          $numberFormmater = new \NumberFormatter('en_GB', \NumberFormatter::CURRENCY);
+          $moneyFormatter = new IntlMoneyFormatter($numberFormmater, $currencies);
+
+          return $moneyFormatter->format($money);
+        });
     }
 }
